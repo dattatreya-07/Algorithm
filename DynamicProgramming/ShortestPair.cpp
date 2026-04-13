2a2c8b3486474b17663698a1fe11358cc2fa87f2
Dattatreya M K <mkannankannanmkannan_bcs28@mepcoeng.ac.in>
2026-04-13 12:13:38 +0530

    Implement Floyd-Warshall algorithm for shortest paths

---
+++ DynamicProgramming/ShortestPair.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+#define INF 77777
+#define MAX 10
+
+int dist[MAX][MAX];
+int n;
+
+void floydWarshall() {
+    for (int k = 0; k < n; k++)
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+}
+
+void printMatrix() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (dist[i][j] == INF)
+                cout << "INF ";
+            else
+                cout << dist[i][j] << "   ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    cout << "Enter number of vertices: ";
+    cin >> n;
+
+    cout << "Enter adjacency matrix:\n";
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> dist[i][j];
+        cout<<endl;
+        printMatrix();
+    floydWarshall();
+
+    cout << "\nShortest distance for vertex:\n";
+    printMatrix();
+
+    return 0;
+}
